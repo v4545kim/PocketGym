b77be1b940387c06d5c256d1938c9c385afea7d9
tjsdla520 <83327352+tjsdla520@users.noreply.github.com>
2021-06-23 19:10:16 +0900

    header,login관련 파일수정 중
    현재 진행중으로 header부분만 수정하고 나머지는 수정이 추가로 필요합니다.

--- src/main/java/member/controller/MemberLoginController.java
+++ src/main/java/member/controller/MemberLoginController.java
@@ -1,20 +1,39 @@
 package member.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import dao.MemberDao;
+import vo.Member;
 
 @Controller
 public class MemberLoginController {
+	@Autowired
+	@Qualifier("mdao")
+	private MemberDao dao;
 	
-	@GetMapping("login.me/get")
+	@GetMapping("/login.me")
 	private String  doGet(Model model){
-		return "";
+		System.out.println("go to login.jsp");
+		return "meLogin";
 	}
 	
-	@PostMapping("login.me/post")
-	private String  doPost(Model model){
-		return "";
+	@PostMapping("/login.me")
+	private String  doPost(
+			@RequestParam(value = "id", required = true) String id,
+			@RequestParam(value = "password", required = true) String password,
+			HttpSession session, Model model){
+		
+		Member bean = this.dao.selectData(id, password);
+		session.setAttribute("loginfo", bean);
+		
+		return "redirect:/main.ma";
 	}
 }
