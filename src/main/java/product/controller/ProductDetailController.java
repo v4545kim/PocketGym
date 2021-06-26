effef2a494ed9d465973004ab54040446a60cf67
v4545kim <v4545kim@naver.com>
2021-06-26 17:39:26 +0900

    쇼핑몰상세페이지-순섭
    상품 상세페이지 작성

--- src/main/java/product/controller/ProductDetailController.java
+++ src/main/java/product/controller/ProductDetailController.java
@@ -1,19 +1,44 @@
 package product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import dao.ProductsDao;
+import vo.Product;
 
 @Controller
 public class ProductDetailController {
 	
-	@GetMapping("prlist.pr/get")
-	private String  doGet(Model model){
-		return "";
+	@Autowired
+	@Qualifier("pdao")
+	private ProductsDao dao;
+	
+	
+	@GetMapping("/prdetail.pr")
+	private ModelAndView  doGet(
+			@RequestParam(value = "pr_id", required = true) int pr_id){
+		ModelAndView mav = new ModelAndView();
+		
+		Product bean = dao.selectDataByPk(pr_id);
+		
+		
+		if(bean != null) {
+			mav.addObject("bean", bean);
+			mav.setViewName("prDetail");
+		} else {
+			mav.setViewName("redirect:/prlist.pr");
+		}
+		
+		return mav;
 	}
 	
-	@PostMapping("prlist.pr/post")
+	@PostMapping("/prdetail.pr")
 	private String  doPost(Model model){
 		return "";
 	}
